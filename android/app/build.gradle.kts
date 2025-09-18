51b6637d6ff669cdc549acf20854fca526fc7e9b
Hossein Pira <158034265+code3-dev@users.noreply.github.com>
2025-09-18 19:00:48 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 31
-        versionName = "3.2.5"
+        versionCode = 33
+        versionName = "3.3.0"
     }
 
     buildTypes {
