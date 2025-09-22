d95d018d1373df91c21260786866b6340ccd0f4b
Hossein Pira <158034265+code3-dev@users.noreply.github.com>
2025-09-22 23:17:10 +0330

    Update app_update.dart

--- lib/models/app_update.dart
+++ lib/models/app_update.dart
@@ -6,7 +6,7 @@ class AppUpdate {
   final String messText;
 
   // Current app version - manually set
-  static const String currentAppVersion = '3.3.0';
+  static const String currentAppVersion = '3.4.0';
 
   AppUpdate({required this.version, required this.url, required this.messText});
 
